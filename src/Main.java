44cb73952a93a30c7e30b723ad4c5d69958fcbc3
Andi Naufal Nurfadhil <111062395+andinaufal120@users.noreply.github.com>
2025-09-24 22:39:44 +0800

    refactor: replace singly LinkedList with Doubly LinkedList in Main class

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import singly.LinkedList;
+import doubly.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,33 +9,45 @@ public class Main {
         list.add(3);
         list.add(4);
         list.add(5);
+        System.out.println(list.get(0).getValue());
+        System.out.println(list.get(1).getValue());
+        System.out.println(list.get(2).getValue());
+        System.out.println(list.get(3).getValue());
 
-        System.out.println("List size: " + list.getSize());
-        System.out.println(list.get(0));
-        System.out.println(list.get(1));
-        System.out.println(list.get(2));
-        System.out.println(list.get(3));
-        System.out.println(list.get(4));
+        System.out.println(list.remove(2).getValue());
+        System.out.println(list.get(0).getValue());
+        System.out.println(list.get(1).getValue());
+        System.out.println(list.get(2).getValue());
+        System.out.println(list.get(3).getValue());
 
-        list.insert(999, 0);
-        System.out.println("List size: " + list.getSize());
-        System.out.println(list.get(0));
-        System.out.println(list.get(1));
-        System.out.println(list.get(2));
-        System.out.println(list.get(3));
-        System.out.println(list.get(4));
-        System.out.println(list.get(5));
+//        list.insert(6, 5);
 
-        System.out.println("The first element is: " + list.getFirst());
-        System.out.println("The last element is: " + list.getLast());
-
-        list.swap(5,2);
-        System.out.println(list.get(0));
-        System.out.println(list.get(1));
-        System.out.println(list.get(2));
-        System.out.println(list.get(3));
-        System.out.println(list.get(4));
+//        System.out.println("List size: " + list.getSize());
+//        System.out.println(list.get(0));
+//        System.out.println(list.get(1));
+//        System.out.println(list.get(2));
+//        System.out.println(list.get(3));
+//        System.out.println(list.get(4));
+//
+//        list.insert(999, 0);
+//        System.out.println("List size: " + list.getSize());
+//        System.out.println(list.get(0));
+//        System.out.println(list.get(1));
+//        System.out.println(list.get(2));
+//        System.out.println(list.get(3));
+//        System.out.println(list.get(4));
 //        System.out.println(list.get(5));
-        System.out.println("The last element is: " + list.getLast());
+//
+//        System.out.println("The first element is: " + list.getFirst());
+//        System.out.println("The last element is: " + list.getLast());
+//
+//        list.swap(5,2);
+//        System.out.println(list.get(0));
+//        System.out.println(list.get(1));
+//        System.out.println(list.get(2));
+//        System.out.println(list.get(3));
+//        System.out.println(list.get(4));
+////        System.out.println(list.get(5));
+//        System.out.println("The last element is: " + list.getLast());
     }
 }
